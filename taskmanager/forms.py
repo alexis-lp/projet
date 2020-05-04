10398a3ac866b3600c2e0f4f3a19b316b9a6d6ad
Alexis Le Penven <alexis.lepenven@gmail.com>
2020-05-04 22:50:55 +0200

    Modification de la méthode graphique pour l'affichage de la page connexion

--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import *
 
 class ConnexionForm(forms.Form):
-    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
+    username = forms.CharField(label="Nom d'utilisateur", max_length=30,widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 
 class TaskForm(forms.ModelForm):
